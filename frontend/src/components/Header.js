6a9d173d3d61b36fb09c230885c3a79ba010ae1f
melee-master <89125019+melee-master@users.noreply.github.com>
2022-06-28 14:17:20 +0530

    third commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,33 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+
 const Header = () => {
     return(
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                <Navbar.Brand href="/">gameHub</Navbar.Brand>
+
+                <Link to='/'>
+                <Navbar.Brand>gameHub</Navbar.Brand>
+                </Link>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart">
+
+                        <Link to='/cart'>
+                        <Nav.Link>
                             <i className="fas fa-shopping-cart"></i>Cart
                         </Nav.Link>
-                        <Nav.Link href="/login">
+                        </Link>
+
+                        <Link to='/login'>
+                        <Nav.Link>
                             <i className="fas fa-user"></i>Sign In
                         </Nav.Link>
+                        </Link>
+
                     </Nav>
                 </Navbar.Collapse>
                 </Container>
