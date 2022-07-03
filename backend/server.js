e4aae68432aa67b5ec62da9be326e70aef16ccd3
melee-master <89125019+melee-master@users.noreply.github.com>
2022-07-04 00:02:07 +0530

    8th commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products')
+dotenv.config();
 const app = express()
-
+const PORT = process.env.PORT || 5500
 app.get('/', (req, res)=>{
     res.send("api is working...");
 })
 app.get('/api/products', (req, res)=>{
     res.json(products);
 })
-app.listen(5500, (req, res)=>{
-    console.log("Server started on port 5500");
+app.listen(PORT, (req, res)=>{
+    console.log(`Server started on port ${PORT}`);
 })
 app.get('/api/products/:id', (req, res)=>{
     const product = products.find((p)=> p._id === req.params.id);
