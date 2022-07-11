ebcb84e3c03366c5f6b9519352565e59e6fb80fa
melee-master <89125019+melee-master@users.noreply.github.com>
2022-07-11 20:34:43 +0530

    error handling and mongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const products = require('./data/products')
+const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5500
 const connectDB  = require('./config/db')
+const productRoutes = require('./routes/productRoutes');
 connectDB();
 const colors = require('colors');
 app.get('/', (req, res)=>{
     res.send("api is working...");
 })
-app.get('/api/products', (req, res)=>{
-    res.json(products);
-})
+app.use('/api/products', productRoutes);
+app.use(notFound)
+app.use(errorHandler)
 app.listen(PORT, (req, res)=>{
     console.log(`Server started on port ${PORT}`.yellow.bold);
-})
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find((p)=> p._id === req.params.id);
-    res.json(product);
 })
